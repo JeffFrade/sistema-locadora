359670e6ff78e203a683c3c5b9fcc1443275a0c7
Jefferson Frade <jefferrson.frade@gmail.com>
2023-05-09 23:00:14 -0300

    Refatorado os Providers

--- config/app.php
+++ config/app.php
@@ -163,8 +163,8 @@ return [
         /*
          * Application Service Providers...
          */
-        App\Providers\AuthServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
+        App\Core\Providers\AuthServiceProvider::class,
+        App\Core\Providers\RouteServiceProvider::class,
     ])->toArray(),
 
     /*
